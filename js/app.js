f8186621089bf735a66a7d9ee0edcb9c020db378
Anh Tuan Tran <nhanhnhanh17399@gmail.com>
2020-02-24 16:50:23 -0500

    Added Sass support

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 import * as wasm from "../pkg/index.js";
+import "../sass/style.sass";
 
 wasm;
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,44 @@
 const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
 const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const dist = path.resolve(__dirname, "dist");
 
 module.exports = {
-  mode: "production",
-  entry: {
-    index: "./js/index.js"
-  },
-  output: {
-    path: dist,
-    filename: "[name].js"
-  },
-  devServer: {
-    contentBase: dist,
-  },
-  plugins: [
-    new CopyPlugin([
-      path.resolve(__dirname, "static")
-    ]),
+    mode: "production",
+    entry: {
+        index: "./js/index.js"
+    },
+    output: {
+        path: dist,
+        filename: "[name].js"
+    },
+    devServer: {
+        contentBase: dist,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new CopyPlugin([
+            path.resolve(__dirname, "static")
+        ]),
 
-    new WasmPackPlugin({
-      crateDirectory: __dirname,
-      extraArgs: "--out-name index"
-    }),
-  ]
+        new WasmPackPlugin({
+            crateDirectory: __dirname,
+            extraArgs: "--out-name index"
+        }),
+
+        new MiniCssExtractPlugin(),
+    ]
 };
