428b24e97e8dcb6acc1b53d72e109b903f3fa0e8
Anh Tuan Tran <nhanhnhanh17399@gmail.com>
2020-04-03 22:27:52 -0400

    Use autoprefixer

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,7 @@ module.exports = {
                 use: [
                     MiniCssExtractPlugin.loader,
                     'css-loader',
+                    "postcss-loader",
                     'sass-loader',
                 ],
             },
